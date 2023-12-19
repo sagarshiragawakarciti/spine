180eeb94eaefeb6c11016a85344e9cd0ae09f5d0
sagarshiragawakarciti <153506921+sagarshiragawakarciti@users.noreply.github.com>
2023-12-19 10:22:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except:
 
 import re, ast
 
-with open('requirements.txt') as f:
-     install_requires = f.read().strip().split('\n')
+#with open('requirements.txt') as f:
+ #    install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in withrun_erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -18,7 +18,7 @@ with open('spine/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+#requirements = parse_requirements("requirements.txt", session="")
 try:
     requirements = [str(ir.req) for ir in install_requires]
 except:
